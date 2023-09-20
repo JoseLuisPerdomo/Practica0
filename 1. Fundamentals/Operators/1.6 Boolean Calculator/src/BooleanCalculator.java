ccd1c6cf510524786d14d674cf6e89a54eefa713
JoseLuis <jose.perdomo109@alu.ulpgc.es>
2023-09-20 18:41:06 +0100

    update Say Hello

--- 1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
+++ 1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
@@ -4,5 +4,11 @@ public class BooleanCalculator
    {
       int a = 3, b = -4, c = 1;
       boolean t = true, f = false;
+       boolean option1 = (a >= b);
+       boolean option2 = -b / a == c;
+       boolean option3 = (t || !f) == (2 * c <= c);
+       boolean option4 = (!t == f) != (b + c / a > a - b * c);
+
+       System.out.print(option1 + " " + option2 + " " + option3 + " " + option4);
    }
 }
