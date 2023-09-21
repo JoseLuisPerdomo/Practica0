49ee1a548d4ab7b672839b92875e159e01f68c55
JoseLuis <jose.perdomo109@alu.ulpgc.es>
2023-09-21 11:04:40 +0100

    update Say Hello

--- 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
+++ 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
@@ -2,6 +2,19 @@ public class HundredthPrime
 {
    public static void main(String[] args)
    {
-      /* Make a code that displays the 100th prime number */
+      int first = 3;
+      int counter = 1;
+
+      while (counter != 100) {
+         for (int divisor = 2; divisor < first; ++divisor) {
+            if (first % divisor == 0) {
+               continue;
+            } else {
+               counter++;
+            }
+            first++;
+         }
+      }
+      System.out.print(first + " is the hundredth prime number.");
    }
 }
